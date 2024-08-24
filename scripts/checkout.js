d7e066581fda415716ee3f14449d8c4a4d29236d
TizianoMeneghetti <tiziano.meneg@gmail.com>
2024-08-24 16:47:40 +0200

    Deleate products from HTML when clicking deleate

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -20,7 +20,8 @@ cart.forEach((cartItem) => {
 
 
   cartSummaryHTML += `
-  <div class="cart-item-container">
+  <div class="cart-item-container 
+  js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">
       Delivery date: Tuesday, June 21
     </div>
@@ -99,6 +100,10 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
     const productID = link.dataset.productId;
     removeFromCart(productID);
 
+    //  Prendo l'elemento usando DOM per cancellarlo
+    const container = document.querySelector(
+      `.js-cart-item-container-${productID}`)
+    container.remove();
 
   });
 })
