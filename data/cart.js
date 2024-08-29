b08df54c1a9c0c03e68e1520e89ee4642c966604
TizianoMeneghetti <tiziano.meneg@gmail.com>
2024-08-29 03:33:18 +0200

    Create automated tests for addCart

--- data/cart.js
+++ data/cart.js
@@ -1,18 +1,23 @@
 
 
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
 
-if (!cart) {
-  cart = [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2,
-    deliveryOptionId: '1'
-  }, {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1,
-    deliveryOptionId: '2'
-  }];
-}
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem('cart'));
+
+  if (!cart) {
+    cart = [{
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2,
+      deliveryOptionId: '1'
+    }, {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1,
+      deliveryOptionId: '2'
+    }];
+  }
+};
+loadFromStorage();
 
 
 // Salvo il carrello in localstorage
@@ -32,7 +37,7 @@ function getProductCart(productId) {
 }
 
 // Controllo se esiste già un ID in cart[], se si incremento q.ta, altrimenti aggiungo 1
-export function addToCart(productId,quantity) {
+export function addToCart(productId, quantity = 1) {
   const matchingItem = getProductCart(productId);
 
   if (matchingItem) {
--- tests-jasmine/utils/moneyTests.js
+++ tests-jasmine/utils/moneyTests.js
@@ -1,4 +1,4 @@
-import { formatCurrency } from "../scripts/utils/money.js"
+import { formatCurrency } from "../../scripts/utils/money.js"
 
 describe('test suite: formatCurrency', () => {
   it('convert cents into dollars', () => {
@@ -8,6 +8,6 @@ describe('test suite: formatCurrency', () => {
     expect(formatCurrency(0)).toEqual('0.00');
   });
   it('round up to nearest cent', () => {
-    expect(formatCurrency(2000.5)).toEqual('20.95');
+    expect(formatCurrency(2000.5)).toEqual('20.01');
   });
 })
