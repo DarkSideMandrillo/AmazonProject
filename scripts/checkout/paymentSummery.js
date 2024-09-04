e2959828964eda2b4e95735d4c3fc9b0aab9cc27
Tiziano Meneghetti <tiziano.meneg@gmail.com>
2024-09-04 17:53:25 +0200

    Use POST for orde - correction problem

--- scripts/checkout/paymentSummery.js
+++ scripts/checkout/paymentSummery.js
@@ -70,13 +70,20 @@ export function renderPaymentSummary() {
     const response = await fetch('https://supersimplebackend.dev/orders', {
       method: 'POST',
       headers: {
-        'Content-Tipe': 'application/json'
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        cart: cart.cartItems
+        cart:  [{
+          productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+          quantity: 2,
+          deliveryOptionId: '1'
+        }, {
+          productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+          quantity: 1,
+          deliveryOptionId: '2'
+        }] //cart.cartItems
       })
     });
-
     const order = await response.json();
     console.log(order);
   });
